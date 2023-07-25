271e1597f6cb43ea34155e53c15839db37645f70
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-07-25 16:46:54 -0500

    N A V B A R P A D D I N G

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <nav className='flex items-center justify-between fixed top-0 left-0 w-full p-4 bg-transparent z-50'>
+    <nav className='flex items-center justify-between fixed top-0 left-0 w-full p-5 bg-transparent z-50'>
       <div className='flex items-center space-x-4'>
         <ul className='flex items-center space-x-4'>
           <li>
